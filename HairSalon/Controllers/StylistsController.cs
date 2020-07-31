7aca7e6f7efd8c6c9ed3787885e23dbbb3de7600
James Henager <jameschenager@gmail.com>
2020-07-31 11:04:27 -0700

    add content to stylists controller

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controllers{
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}
